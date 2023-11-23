4589556f8092984a3538f35502df27865e59cfcd
knakna <kna@tut.by>
2023-11-23 22:42:43 +0300

    add function multiply

---
+++ 8kyu/multiply.js
@@ -0,0 +1,13 @@
+// ✨DESCRIPTION: (win + :  для смайла)
+// This code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//     a * b
+//   }
+
+
+// ✔solution
+
+function multiply(a, b){
+    return a * b
+  }
+
